1b26214fd38f71784193255beaea73b7a8c16ea1
Shankho Boron Ghosh <shankhoghosh123@gmail.com>
2020-06-18 09:05:14 +0530

    BasicAGL Demo

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -64,9 +64,9 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href="#">Try It Out</Button>
+            <Button href={docUrl('abstract.html')}>Example Link</Button>
+            <Button href={docUrl('documentation-usage.html')}>Example Link 2</Button>
           </PromoSection>
         </div>
       </SplashContainer>
